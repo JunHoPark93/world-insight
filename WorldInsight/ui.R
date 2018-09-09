6ade44313372bd807ce93c2eaf8c36969a35881d
pjh374968@gmail.com <pjh374968@gmail.com>
2018-09-09 21:42:31 +0900

    Add tabPanel worldmap to UI

--- WorldInsight/ui.R
+++ WorldInsight/ui.R
@@ -17,11 +17,19 @@ navbarPage("Superzip", id="nav",
            
            tabPanel("World map",
                     div(class="outer",
-                        leafletOutput("map", width="100%", height="100%")
+                        leafletOutput("map", width="100%", height="100%"),
                         
-                        
-                    )
-           )
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 60, left = "auto", right = 20, 
+                                      bottom = "auto",width = 330, height = "auto",
+                                      
+                                      h2("World Map (세계지도)"),
+                                      
+                                      selectInput("period", "연도", periodGlobal, selected = "2000"),
+                                      selectInput("prodName", "품목", prodName, selected = "살아 있는 동물"), 
+                                      selectInput("option", "옵션", option, selected = "imprtMny")))
+                              
+           ) # end of tab Panel (World Map)
                         
                       
 )
